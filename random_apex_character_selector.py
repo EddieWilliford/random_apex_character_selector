e4124c768bab31320036f2377a1a0c34f4ed0d19
EddieWilliford <102650998+EddieWilliford@users.noreply.github.com>
2022-12-05 10:06:00 -0700

    Add files via upload

---
+++ random_apex_character_selector.py
@@ -0,0 +1,9 @@
+import random
+
+names = ['Bloodhound', 'Gibraltar', 'lifeline', 'pathfinder', 'wraith', 'bangalore', 'caustic', 'mirage', 'octane', 'wattson', 'crypto', 'revenant', 'loba', 'rampart', 'horizon', 'fuse', 'valkyrie', 'seer', 'ash', 'mad maggie', 'newcastle', 'vantage', 'cataylst']
+character = random.choice(names)
+print(character)
+
+names2 = ['Bloodhound', 'Gibraltar', 'lifeline', 'pathfinder', 'wraith', 'bangalore']
+character2 = random.choice(names2)
+print(character2)
